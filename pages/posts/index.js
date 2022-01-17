14c4fabdbcc9a4262e38c603d061b42bdb52ae7b
1046andwhite@gmail.com <1046andwhite@gmail.com>
2022-01-18 06:21:35 +0900

    fix: index.jsを修正

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,4 @@
-import { getSortedPostsData } from "../lib/posts";
+import { getSortedPostsData } from "../../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
