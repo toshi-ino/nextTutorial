fa80c6a042197adb15f49cc915b0184252f3c14d
1046andwhite@gmail.com <1046andwhite@gmail.com>
2022-01-19 06:14:23 +0900

    fix: next.confignを変更

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false;
-      config.resolve.fallback.child_process = false;
-      config.resolve.fallback.net = false;
-      config.resolve.fallback.dns = false;
-      config.resolve.fallback.tls = false;
+      config.node = {
+        fs: "empty",
+      };
     }
     return config;
   },
